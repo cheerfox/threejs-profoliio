a44c772e5e5c323129d25824a51996b6e105ddfa
Chun-Chi Wang <limitsea0103@gmail.com>
2024-09-21 21:31:01 +0800

    Add Navbar and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
   return (
       <main className="max-w-7xl mx-auto">
-        <h1 className="text-2xl text-white underline">Hello, Three JS!!</h1>
+        <Navbar />
+        <Hero />
       </main>
   )
 }
